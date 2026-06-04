0cbdae59eb2bac2b5b84ef5104b4de0ed934b542
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2026-06-05 01:29:28 +0300

    add app exceptions

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,14 @@
+import 'package:movie_app/core/network/error_message_model.dart';
+
+class ServerException implements Exception {
+  final ErrorMessageModel errorMessageModel;
+
+  ServerException({required this.errorMessageModel});
+}
+
+
+class LocalDatabaseException implements Exception {
+  final String errorMessage;
+
+  LocalDatabaseException({required this.errorMessage});
+}
