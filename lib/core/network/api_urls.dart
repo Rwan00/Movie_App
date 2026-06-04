c9945d5898a723790aafae0931af21b634440838
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2026-06-05 01:55:59 +0300

    add noe plating movies datasource

---
+++ lib/core/network/api_urls.dart
@@ -0,0 +1,9 @@
+class ApiUrls {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "392384457cd4710976dceb965ff1c211";
+
+  static String endpointPath(String endpoint) =>
+      "$baseUrl/$endpoint?api_key=$apiKey";
+
+  static const String nowPlayingMovies = "/movie/now_playing";
+}
---
+++ lib/movies/data/datasource/movie_remote_datasource.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:movie_app/core/error/exceptions.dart';
+import 'package:movie_app/core/network/api_urls.dart';
+import 'package:movie_app/core/network/error_message_model.dart';
+import 'package:movie_app/movies/data/models/movie_model.dart';
+
+abstract class BaseMovieRemoteDatasource {
+  Future<List<MovieModel>> getNowPlayingMovies();
+}
+
+class MovieRemoteDatasource implements BaseMovieRemoteDatasource{
+
+  @override
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get(
+      ApiUrls.endpointPath(ApiUrls.nowPlayingMovies),
+    );
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from(
+        (response.data["results"] as List).map((e) => MovieModel.fromJson(e)),
+      );
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+}
